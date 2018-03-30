a68d1bdf48ebd8618d26d83793c8f849e89d6f90
xiaolu5 <xiaolu5@staff.sina.com.cn>
2018-03-30 16:15:36 +0800

    首次提交

---
+++ BBATest/BBAHelper.h
@@ -0,0 +1,13 @@
+//
+//  BBAHelper.h
+//  BBATest
+//
+//  Created by xiaolu5 on 2018/3/30.
+//  Copyright © 2018年 xiaolu5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBAHelper : NSObject
++ (void)test;
+@end
---
+++ BBATest/BBAHelper.m
@@ -0,0 +1,16 @@
+//
+//  BBAHelper.m
+//  BBATest
+//
+//  Created by xiaolu5 on 2018/3/30.
+//  Copyright © 2018年 xiaolu5. All rights reserved.
+//
+
+#import "BBAHelper.h"
+
+@implementation BBAHelper
++ (void)test{
+    NSLog(@"test");
+    
+}
+@end
